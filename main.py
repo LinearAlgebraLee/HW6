bfa524dd48673f3835149cf4f6c6cfac7646d418
LinearAlgebraLee <56056684+LinearAlgebraLee@users.noreply.github.com>
2019-12-12 17:36:57 +0800

    save plots

--- main.py
+++ main.py
@@ -9,14 +9,11 @@ def main():
     ks = [1, 5, 50, 150, 400, 1050, 1289]
     err = []
     for k in ks:
-        print("Perform SVD for k=%d ..." % k, end='\r')
+        print("Perform SVD for k=%d ..." % k)
         imArr_compressed = svd_compress(imArr, K=k)
         err += [approx_error(imArr, imArr_compressed)]
         save_image(imArr_compressed, 'result_{}.jpg'.format(k))
-
-    plt.imshow(imArr_compressed)
-    plt.show()
-    plot_curve(ks, err, show=True)
+    plot_curve(ks, err, show=False)
 
 if __name__ == '__main__':
     main()
